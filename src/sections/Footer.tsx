be99fdcc6635637c8f08175c71640344ea416de3
Fabien <hombert.fabien@gmail.com>
2024-09-20 19:08:21 +0200

    dernier dernier push

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,9 +2,24 @@ const Footer = () => {
   return (
     <section className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
       <div className="flex gap-3">
-        <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
+        <a href="https://github.com/Picoche" target="_blank">
+          <div className="social-icon">
+            <img
+              src="/assets/github.svg"
+              alt="github"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/fabien-hombert/" target="_blank">
+          <div className="social-icon">
+            <img
+              src="/assets/linkedin.png"
+              alt="linkedin"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
       </div>
       <p className="text-white-500">
         © 2024 Fabien Hombert. Tous droits réservés.
