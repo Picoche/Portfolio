b38a205732eae8d03e435b7af4f34e327bcaa4d7
Fabien <hombert.fabien@gmail.com>
2024-11-28 19:04:48 +0100

    fix animation

--- src/components/sections/Hero/FirstHero.tsx
+++ src/components/sections/Hero/FirstHero.tsx
@@ -123,7 +123,7 @@ export function FirstHero() {
           {[...Array(50)].map((_, i) => (
             <div
               key={i}
-              className="floating-particle absolute bg-accent/10 dark:bg-background opacity-10 rounded-full"
+              className="floating-particle absolute bg-secondary dark:bg-background opacity-10 rounded-full"
               style={{
                 width: Math.random() * 20 + 10,
                 height: Math.random() * 20 + 10,
